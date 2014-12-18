1ffe8162c9c09220d967428c381fe81697d93d58
Michael Robinson <michael.robinson@kiwirail.co.nz>
2014-12-19 08:52:24 +1300

    Updated angular module interface

--- interfaces/angular.js
+++ interfaces/angular.js
@@ -1,6 +1,10 @@
+declare class AngularJSModule {
+  controller(name: string, dependencies: Array<Function | string>): AngularJSModule;
+}
+
 declare class AngularJS {
   copy(object: Object | Array): Object | Array;
-  module(name: string, dependencies: Array<number | string>): Object;
+  module(name: string, dependencies: Array<string>): AngularJSModule;
 }
 
 declare var angular: AngularJS;
