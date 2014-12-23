efdefed35f954bab7a924b7871ba5435aa895ea7
Michael Robinson <michael.robinson@kiwirail.co.nz>
2014-12-23 14:06:13 +1300

    Added factory, sort methods alphabetically

--- interfaces/angular.js
+++ interfaces/angular.js
@@ -2,34 +2,36 @@ declare var AngularJSIterable: Object | Array;
 declare var AngularJSStringNumber: string | number;
 
 declare class AngularJSCallbacks {
-  iterator(value: any, key: string | number): void;
   css(index: number, value: string): AngularJSStringNumber;
+  iterator(value: any, key: string | number): void;
 }
 
 declare class AngularJSJQueryLite {
   bind(eventType: string, handler: Function): AngularJSJQueryLite;
+  css(properties: Object): AngularJSJQueryLite;
+  css(propertyName: string): string;
+  css(propertyName: string, propertyFunction: AngularJSCallbacks.css): AngularJSJQueryLite;
+  css(propertyName: string, value: string): AngularJSJQueryLite;
   unbind(eventType: string): AngularJSJQueryLite;
   unbind(eventType: string, handler: Function): AngularJSJQueryLite;
-  css(propertyName: string): string;
-  css(propertyName: string, value: string): AngularJSJQueryLite;
-  css(propertyName: string, propertyFunction: AngularJSCallbacks.css): AngularJSJQueryLite;
-  css(properties: Object): AngularJSJQueryLite;
 }
 
 declare class AngularJSModule {
   controller(name: string, dependencies: Array<Function | string>): AngularJSModule;
-  directive(name: string, directiveFactory: Array<Function | string>):AngularJSModule;
-  directive(name: { name: Array<Function | string> }):AngularJSModule;
+  directive(name: string, directiveFactory: Array<Function | string>): AngularJSModule;
+  directive(name: { name: Array<Function | string> }): AngularJSModule;
+  factory(name: string, providerFunction: Array<Function | string>): AngularJSModule;
+  factory(name: string, providerFunction: Function): AngularJSModule;
   value(name: string, object: any): AngularJSModule;
 }
 
 declare class AngularJS {
   copy(object: Object | Array): Object | Array;
-  extend<T>(dst: T, src: Object): T;
-  module(name: string, dependencies: Array<string>): AngularJSModule;
   element(element: string | Element): AngularJSJQueryLite;
+  extend<T>(dst: T, src: Object): T;
   forEach(obj: AngularJSIterable, iterator: AngularJSCallbacks.iterator): AngularJSIterable;
   forEach(obj: AngularJSIterable, iterator: AngularJSCallbacks.iterator, context: Object): AngularJSIterable;
+  module(name: string, dependencies: Array<string>): AngularJSModule;
 }
 
 declare var angular: AngularJS;
