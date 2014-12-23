d297aa3fb95d932466aeb6f04df3ab47c46fc699
Michael Robinson <michael.robinson@kiwirail.co.nz>
2014-12-23 15:44:33 +1300

    Added alternative $http get

--- interfaces/_http.js
+++ interfaces/_http.js
@@ -1,6 +1,7 @@
 /* jshint ignore: start */
 declare class AngularJS_HTTP {
-  get(url: string): Promise
+  get(url: string): Promise;
+  get(url: string, config: Object): Promise;
 }
 
 declare var $http: AngularJS_HTTP;
